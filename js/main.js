6bb5efddd9b0005f32707f9713716f68db801f56
Dominik Balazka <74915142+balazka@users.noreply.github.com>
2024-08-10 18:42:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,11 +52,11 @@ function initSigma(config) {
 	else
 		drawProps={
         defaultLabelColor: "#000",
-        defaultLabelSize: 14,
+        defaultLabelSize: 8,
         defaultLabelBGColor: "#ddd",
         defaultHoverLabelBGColor: "#002147",
         defaultLabelHoverColor: "#fff",
-        labelThreshold: 10,
+        labelThreshold: 12,
         defaultEdgeType: "curve",
 	defaultEdgeColor: "rgba(211, 211, 211, 0.5)", // Set custom default edge color
         hoverFontStyle: "bold",
